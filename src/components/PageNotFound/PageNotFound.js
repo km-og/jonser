9c1dbe842ec1eddd0a17705637a3fcdbcafb7c2e
Inessa Kim <kim.inessa20@yandex.ru>
2024-01-25 15:42:19 +0300

    fix: добавлена страница 404, изменено фикс меню при скролле, изменила логотипы

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+import "./PageNotFound.css";
+
+function PageNotFound() {
+  return (
+    <section className="not-found">
+      <div className="not-found__container">
+        <h2 className="not-found__title">404</h2>
+        <p className="not-found__text">Страница не найдена</p>
+        <NavLink to="/" className="not-found__link link">
+          Вернуться на главную страницу
+        </NavLink>
+      </div>
+    </section>
+  );
+}
+
+export default PageNotFound;
